ca7cffc755f0ca35580b20c172978cf9c703f5d6
Mostafa bagheri <bgr.mostafa@outlook.com>
2024-11-08 18:51:50 +0330

    add download viewmodel

---
+++ src/Client/Client.UI/ViewModel/Download/DownloadViewModel.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Client.UI.ViewModel.Download;
+
+public class DownloadViewModel
+{
+    [DisplayName("شناسه")]
+    public int Id { get; set; }
+    [DisplayName("نام  فایل")]
+    public string FileName { get; set; }
+    [DisplayName("آیکن")]
+    public string FileIcon { get; set; }
+    [DisplayName("صف")]
+    public string DownloadQueue { get; set; }
+    [DisplayName("اندازه")]
+    public int Size { get; set; }
+    [DisplayName("وضعیت")]
+    public string Status { get; set; }
+    [DisplayName("پیشرفت")]
+    public int Percent { get; set; }
+    [DisplayName("سرعت دانلود")]
+    public int Speed { get; set; }
+    [DisplayName("زمان باقیمانده")]
+    public int Remain { get; set; }
+    [DisplayName("آخرین تاریخ دانلود")]
+    public DateTime LatestDownloadDateTime { get; set; }
+    [DisplayName("توضیحات")]
+    public string Description { get; set; }
+}
